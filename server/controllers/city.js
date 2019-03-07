fb2b7d6e86f8f93300999fcb3b4feb2adc9a9592
Heim <heimzeng@gmail.com>
2019-03-07 15:08:30 +0800

    根据新接口添加代码
    具体为：添加开始的位置权限获取

--- server/controllers/city.js
+++ server/controllers/city.js
@@ -1,9 +1,10 @@
 const DB = require('./dbindex')
 
+//根据provinceId获取该province的cities
 module.exports = {
   get: async (ctx, next) => {
     var result = await DB.raw('SELECT * FROM city WHERE province_id = ' + ctx.request.query.provinceId).then((res) => {
-      return res
+      return res;
     })
     ctx.state.data = {
       resultCode: 0,
